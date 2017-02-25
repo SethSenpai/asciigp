cfb894c59668c3f64cc1185b33e6d1ca09cfaa12
pimOstendorf <pimwing@gmail.com>
2017-02-25 18:00:02 +0100

    initial commit

---
+++ asciipg/asciipg.cpp
@@ -0,0 +1,18 @@
+// asciipg.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    
+	cout << "Welcome to the dungeon adventurer!" << endl << "===================================" << endl;
+
+	return 0;
+}
+
